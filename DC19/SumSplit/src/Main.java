ea6245e99c93ea01731e675ba4485c47db45ca9f
Krister <SeventhDisaster@outlook.com>
2019-03-18 07:39:31 +0100

    Daily Challenge 19 initial commit

---
+++ DC19/SumSplit/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isThereBalance(new int[]{1,1,1,2,1}));
+        System.out.println(isThereBalance(new int[]{2,1,1,2,1}));
+        System.out.println(isThereBalance(new int[]{15,15}));
+    }
+
+    public static boolean isThereBalance(int[] arr){
+        //Iterate through array
+        //i determines the split point
+        for(int i = 0; i < arr.length; i++){
+            // Variable for the values before the split
+            int firstHalf = 0;
+            for (int a = 0; a <= i; a++){
+                // Summarize numbers from 0 to split point
+                firstHalf += arr[a];
+            }
+
+            // Variable for the values after the split
+            int secondHalf = 0;
+            for (int b = arr.length - 1; b > i; b--){
+                // Summarize numbers from end to split point
+                secondHalf += arr[b];
+            }
+
+            // If the two values can summarize to the same number at any point, return true
+            if (firstHalf == secondHalf){
+                return true;
+            }
+        }
+
+        // Return true never happens, they can't summarize to equal values, return false.
+        return false;
+    }
+}
