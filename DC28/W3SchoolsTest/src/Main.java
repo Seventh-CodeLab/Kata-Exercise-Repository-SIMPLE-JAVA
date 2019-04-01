c6bd34f8fc292f3ddfc540cb22642d34b872e317
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-04-01 14:27:27 +0200

    Daily challenge 28 initial commit Nocodehere

---
+++ DC28/W3SchoolsTest/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //This task didn't have any concrete objective.
+        //We completed a test on W3Schools following this link:
+
+        //TODO: https://www.w3schools.com/java/java_exercises.asp
+
+        //I completed the test, and the result is seen in CompletedTest.png
+    }
+}
