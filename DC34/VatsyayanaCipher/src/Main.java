3ea9f9dca6379c9b0dd073b7d543026abcf88225
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-04-10 16:57:15 +0200

    Modified main for dynamic input

--- DC34/VatsyayanaCipher/src/Main.java
+++ DC34/VatsyayanaCipher/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //Below is code with preset values instead of console input
+        /*
         // I set a boolean for printing the table if successfull or not
         VatsyayanaCipher.generateTable("THEQUICKBROWN", "FXJMPSVLAZYDG",true); //Successful Generation
         System.out.println();
@@ -19,5 +23,32 @@ public class Main {
         //Runs a the method again but this time with an error due to a missing letter in the table
         String[] badTable = VatsyayanaCipher.generateTable("THQUICKBROWN", "FXMPSVLAZYDG"); //Create a table missing E and J
         System.out.println(VatsyayanaCipher.cipher("MEET AT ELEVEN", badTable)); //Try to cipher and decipher
+        */
+
+        String[] table = inTable();
+        VatsyayanaCipher.generateTable(table[0],table[1],printIt());
+        System.out.println(VatsyayanaCipher.cipher(input(),table));
+
+    }
+
+    static String input(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("== Input the text you want to encrypt ==");
+        return scan.nextLine();
+    }
+
+    static String[] inTable(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("== Input first key string ==");
+        String key1 = scan.nextLine();
+        System.out.println("== Input second key string ==");
+        String key2 = scan.nextLine();
+        return new String[]{key1,key2};
+    }
+
+    static boolean printIt(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("== Do you want to print the table? true/false ==");
+        return scan.nextBoolean();
     }
 }
