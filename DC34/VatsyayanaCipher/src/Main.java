2b59a6d0a0ede84b90f499caecdbc45b96d49b8f
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-04-11 13:52:25 +0200

    Adjusted main commenting

--- DC34/VatsyayanaCipher/src/Main.java
+++ DC34/VatsyayanaCipher/src/Main.java
@@ -4,25 +4,25 @@ public class Main {
     public static void main(String[] args) {
         //Below is code with preset values instead of console input
         /*
-        // I set a boolean for printing the table if successfull or not
-        VatsyayanaCipher.generateTable("THEQUICKBROWN", "FXJMPSVLAZYDG",true); //Successful Generation
-        System.out.println();
-        VatsyayanaCipher.generateTable("THIS CIPHER", "WILLNOTWORK",true); //Error: Contains duplicates
-        System.out.println();
-        VatsyayanaCipher.generateTable("THISWILLNOTWORKEITHER","RIGHT",true); //Error: Different Lengths
-
-
-        //Generates the sample working table without printing
-        String[] table = VatsyayanaCipher.generateTable("THEQUICKBROWN", "FXJMPSVLAZYDG");
-
-        //This method both encrypts AND decrypts a functional generated table.
-        System.out.println(VatsyayanaCipher.cipher("MEET AT ELEVEN", table));
-        System.out.println(VatsyayanaCipher.cipher("QJJF BF JKJCJG", table));
-
-
-        //Runs a the method again but this time with an error due to a missing letter in the table
-        String[] badTable = VatsyayanaCipher.generateTable("THQUICKBROWN", "FXMPSVLAZYDG"); //Create a table missing E and J
-        System.out.println(VatsyayanaCipher.cipher("MEET AT ELEVEN", badTable)); //Try to cipher and decipher
+        * //I set a boolean for printing the table if successfull or not
+        * VatsyayanaCipher.generateTable("THEQUICKBROWN", "FXJMPSVLAZYDG",true); //Successful Generation
+        * System.out.println();
+        * VatsyayanaCipher.generateTable("THIS CIPHER", "WILLNOTWORK",true); //Error: Contains duplicates
+        * System.out.println();
+        * VatsyayanaCipher.generateTable("THISWILLNOTWORKEITHER","RIGHT",true); //Error: Different Lengths
+        *
+        *
+        * //Generates the sample working table without printing
+        * String[] table = VatsyayanaCipher.generateTable("THEQUICKBROWN", "FXJMPSVLAZYDG");
+        *
+        * //This method both encrypts AND decrypts a functional generated table.
+        * System.out.println(VatsyayanaCipher.cipher("MEET AT ELEVEN", table));
+        * System.out.println(VatsyayanaCipher.cipher("QJJF BF JKJCJG", table));
+        *
+        *
+        * //Runs a the method again but this time with an error due to a missing letter in the table
+        * String[] badTable = VatsyayanaCipher.generateTable("THQUICKBROWN", "FXMPSVLAZYDG"); //Create a table missing E and J
+        * System.out.println(VatsyayanaCipher.cipher("MEET AT ELEVEN", badTable)); //Try to cipher and decipher
         */
 
         String[] table = inTable();
@@ -31,13 +31,13 @@ public class Main {
 
     }
 
-    static String input(){
+    private static String input(){
         Scanner scan = new Scanner(System.in);
         System.out.println("== Input the text you want to encrypt ==");
         return scan.nextLine();
     }
 
-    static String[] inTable(){
+    private static String[] inTable(){
         Scanner scan = new Scanner(System.in);
         System.out.println("== Input first key string ==");
         String key1 = scan.nextLine();
@@ -46,7 +46,7 @@ public class Main {
         return new String[]{key1,key2};
     }
 
-    static boolean printIt(){
+    private static boolean printIt(){
         Scanner scan = new Scanner(System.in);
         System.out.println("== Do you want to print the table? true/false ==");
         return scan.nextBoolean();
