6816ff09d23dbf438ffb8b523b9bb46fe6125b9a
Krister <SeventhDisaster@outlook.com>
2019-04-15 21:01:11 +0200

    Added Misc folder and StupidSort algorithm

---
+++ Misc/StupidSort/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/*
+This is a "Stupid" sorting algorithm that basically randomly generates a new index position for every value inside of
+an array and assigns each new value to a new position every time, checks if it's sorted, and returns it if it is.
+
+Obviously a horribly inefficient method of sorting arrays, especially if they are big, but fun nonetheless.
+
+See the StupidSort.java file for the actual code.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(StupidSort.stupidSort(new int[]{16,18,3,7,24,17,10,2})));
+    }
+}
---
+++ Misc/StupidSort/src/StupidSort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class StupidSort {
+    static int iteration = 0;
+
+    static int[] stupidSort(int[] arr){
+        int[] sortedArr = randomSort(arr);
+        while (!isSorted(sortedArr)){
+            sortedArr = randomSort(arr);
+        }
+        return sortedArr;
+    }
+
+    //Checks if the array is sorted
+    private static boolean isSorted(int[] arr){
+        System.out.println("Iteration "+ iteration + ": " + Arrays.toString(arr));
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] < arr[i-1]){
+                //If even one value is smaller than the one that comes before it, it's not sorted
+                iteration++;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Actual sorting algorithm
+    private static int[] randomSort(int[] arr){
+        Random rand = new Random();
+        int[] indexes = new int[arr.length];
+
+        //First assign all values in indexes to something outside index bounds to prevent infinity
+        for(int i = 0; i < indexes.length; i++){
+            indexes[i] = arr.length+1;
+        }
+
+        //Assign indexes randomly to a separate index array
+        for(int i = 0; i < indexes.length; i++){
+            int randomIndex = rand.nextInt(arr.length);
+            while (indexUsed(indexes,randomIndex)){
+                //Keep making indexes until you get an unused one
+                randomIndex = rand.nextInt(arr.length);
+            }
+            indexes[i] = randomIndex;
+        }
+
+        int[] outputArr = new int[arr.length];
+        for(int i = 0; i < outputArr.length; i++){
+            outputArr[indexes[i]] = arr[i];
+        }
+        return outputArr;
+    }
+
+    private static boolean indexUsed(int[] indexes, int currentIndex){
+        for(int index : indexes){
+            if(index == currentIndex){
+                return true;
+            }
+        }
+        return false;
+    }
+}
